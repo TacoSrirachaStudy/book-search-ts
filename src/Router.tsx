8295501eff1e10b463db8230ab4d99dbfb6bafac
HAE2NI <hae0_02ni@ewhain.net>
2023-12-29 22:08:41 +0900

    [ feat ] router 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Error from '@components/Error';
-import Layout from '@components/Layout';
-import Main from '@pages/Main';
+import App from './App';
+import Search from '@pages/Search';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <App />,
     errorElement: <Error />,
-    children: [{ index: true, element: <Main /> }],
+    children: [{ index: true, element: <Search /> }],
   },
 ]);
