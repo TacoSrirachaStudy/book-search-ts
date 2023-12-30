98e67628e2f5eaf735367900ca2ddf5ec6aeac52
HAE2NI <hae0_02ni@ewhain.net>
2023-12-30 21:50:44 +0900

    [ feat ] 검색 시 data 나타나게 설정

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,33 +1,62 @@
-import useSWR, { useSWRConfig } from 'swr';
+import useSWR from 'swr';
+import styled from '@emotion/styled';
 import SearchWrapper from './components/SearchWrapper';
 import { fetcher } from '@utils/fetcher';
 import { useState } from 'react';
+import Books from './components/Books';
+import { commonFlex } from '@styles/common';
+import { Book } from 'types/books';
 
 // 검색화면입니다.
 export default function Search() {
   const [userInput, setUserInput] = useState('');
   const [confirmValue, setConfirmValue] = useState('');
 
-  const { data, error, isLoading } = useSWR(
-    `https://dapi.kakao.com/v3/search/book?query=${confirmValue}`,
+  const { data } = useSWR(
+    confirmValue ? `https://dapi.kakao.com/v3/search/book?query=${confirmValue}` : null,
     fetcher
   );
 
   return (
-    <>
+    <Container>
       <SearchWrapper
         userInput={userInput}
         setUserInput={setUserInput}
         confirmValue={confirmValue}
         setConfirmValue={setConfirmValue}
       />
-      <button
-        onClick={() => {
-          console.log(data.documents);
-        }}
-      >
-        흑흑
-      </button>
-    </>
+
+      <BooksContainer>
+        {data &&
+          data.map((book: Book) => {
+            const { authors, contents, title, isbn, thumbnail } = book;
+            return (
+              <Books
+                key={isbn}
+                title={title}
+                thumbnail={thumbnail}
+                authors={authors}
+                contents={contents}
+              />
+            );
+          })}
+      </BooksContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  ${commonFlex}
+  flex-direction: column;
+`;
+
+const BooksContainer = styled.section`
+  ${commonFlex}
+  flex-direction: column;
+  gap: 4rem;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: ${({ theme }) => theme.colors.bgColor};
+`;
