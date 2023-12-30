1c08d59890f8c4af8ae70a34902adbc538309641
HAE2NI <hae0_02ni@ewhain.net>
2023-12-30 21:03:18 +0900

    [ feat ] api fetching

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,10 +1,33 @@
+import useSWR, { useSWRConfig } from 'swr';
 import SearchWrapper from './components/SearchWrapper';
+import { fetcher } from '@utils/fetcher';
+import { useState } from 'react';
 
 // 검색화면입니다.
 export default function Search() {
+  const [userInput, setUserInput] = useState('');
+  const [confirmValue, setConfirmValue] = useState('');
+
+  const { data, error, isLoading } = useSWR(
+    `https://dapi.kakao.com/v3/search/book?query=${confirmValue}`,
+    fetcher
+  );
+
   return (
     <>
-      <SearchWrapper />
+      <SearchWrapper
+        userInput={userInput}
+        setUserInput={setUserInput}
+        confirmValue={confirmValue}
+        setConfirmValue={setConfirmValue}
+      />
+      <button
+        onClick={() => {
+          console.log(data.documents);
+        }}
+      >
+        흑흑
+      </button>
     </>
   );
 }
