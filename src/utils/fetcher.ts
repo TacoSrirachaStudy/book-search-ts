b6f7ef421718207fafb7fb67c4a335bb43833ee4
HAE2NI <hae0_02ni@ewhain.net>
2023-12-30 21:05:34 +0900

    [ feat ] fetcher 함수 추가

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const fetcher = async (url: string) => {
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `${import.meta.env.VITE_REST_API_KEY}`,
+    },
+  });
+  return response.data;
+};
